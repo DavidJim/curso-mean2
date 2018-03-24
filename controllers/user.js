339308ddfb2dc26872fc4307fb51906309414c81
David Jiménez <davidjim1993@gmail.com>
2018-03-24 19:38:56 +0100

    Commit inicialización

---
+++ controllers/user.js
@@ -0,0 +1,56 @@
+'user strict'
+var bcrypt = require('bcrypt-nodejs');
+var User = require('../models/user');
+
+function pruebas(req, res){
+	res.status(200).send({
+		message: 'Probando acción de controlador de usuarios del api rest con Node y Mongo'
+	});
+}
+
+function saveUser(req,res){
+	var user = new User();
+
+	var params = req.body;
+
+	console.log(params);
+
+	user.name = params.name;
+	user.surname = params.surname;
+	user.email = params.email;
+	user.role = 'ROLE_ADMIN';
+	user.image = 'null';
+
+	if(params.password){
+		//Encriptar y guardar
+		bcrypt.hash(params.password, null, null, function(err, hash){
+			user.password = hash;
+			if(user.name != null && user.surname != null && user.email != null){
+				//Guardar usuario
+				user.save((err, userStored) => {
+					if(err){
+						res.status(500).send({message: 'Error al guardar el usuario'});
+					}else{
+						if(!userStored){
+							res.status(404).send({message: 'No se ha guardado el usuario'});
+						}else{
+							res.status(200).send({user: userStored});
+						}
+					}
+				})
+
+			}else{
+				res.status(200).send({message: 'Rellena todos los campos'});
+
+			}
+		})
+	}else{
+		res.status(200).send({message: 'Introduce la contraseña'});
+	}
+
+}
+
+module.exports = {
+	pruebas,
+	saveUser
+};
